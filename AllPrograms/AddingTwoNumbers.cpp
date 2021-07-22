c767559f21fee64357ff52b52a376b822dd32962
Suraj <navneetraj07903@gmail.com>
2021-07-22 23:22:20 +0530

    First Commit

---
+++ AllPrograms/AddingTwoNumbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x,y,sum;
+  cout<<"Program to add two numbers."<<endl;
+  cout<<"Enter first number:";
+  cin>> x;
+  cout<<"Enter 2nd number:";
+  cin>> y;
+  sum = x + y;
+  cout<<"Sum of "<< x << " and " <<y<< " is " <<sum<<endl;
+  return 0;
+} 
---
+++ AllPrograms/AddingTwoNumbers.py
@@ -0,0 +1,6 @@
+print("Program to add two numbers!");
+num1=int(input("Enter first number:"));
+num2=int(input("Enter 2nd number:"));
+sum = num1 + num2;
+print("The sum of two numbers is: ",sum);
+
